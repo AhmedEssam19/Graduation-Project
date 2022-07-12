58f143b59fa3a2fb0e85bcba8323dba6abb27e5d
Ahmed Essam <ahmed.elhlwgy@gmail.com>
2022-07-12 17:18:20 +0200

    Add Setup Script

---
+++ deployment/setup.sh
@@ -0,0 +1,10 @@
+apt update
+apt-get install libpng-dev libjpeg-dev
+pip3 install -U pip
+python3 -m pip install cmake packaging
+python3 -m pip install python-can dlib==19.23.0 imutils scipy opencv-python opencv-contrib-python
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+cd torch2trt
+python3 setup.py install
+cd ..
+rm -r torch2trt
